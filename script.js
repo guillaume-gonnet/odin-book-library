6caf321fc79791383710c98cd9efafd052fe371c
Guillaume Gonnet <111474584+guillaume-gonnet@users.noreply.github.com>
2023-12-24 07:55:20 -0500

    implement class

--- script.js
+++ script.js
@@ -1,21 +1,70 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.id = null;
-    this.info = function () { return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}` }
-    this.toggleRead = function () { this.read = !this.read }
-}
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.id = null;
+        this.info = function () { return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}` }
+        this.toggleRead = function () { this.read = !this.read }
+    }
 
-function addBookToLibrary(book) {
-    const id = myLibrary.length;
-    book.id = id;
-    myLibrary.push(book);
-    console.log("book added to library:" + book.info());
-    addBookToTable(book);
+    addBookToLibrary() {
+        const id = myLibrary.length;
+        this.id = id;
+        myLibrary.push(this);
+        console.log("book added to library:" + this.info());
+        this.addBookToTable();
+    }
+
+    addBookToTable() {
+        const bookInfo = ["title", "author", "pages", "read"];
+        const tableBody = document.getElementById("bookTableBody");
+        const tr = document.createElement("tr");
+
+        for (let i = 0; i < bookInfo.length - 1; i++) {
+            const td = document.createElement("td");
+            td.innerText = this[bookInfo[i]];
+            tr.appendChild(td);
+        }
+        //create button read
+        const btnRead = document.createElement('button');
+        btnRead.innerText = this.read ? "Read" : "Not Read";
+        btnRead.setAttribute("class", this.read ? "read" : "not-read");
+        btnRead.setAttribute('data-index-number', this.id);
+        btnRead.addEventListener('click', (e) => {
+            this.read ? btnRead.classList.replace("read", "not-read") : btnRead.classList.replace("not-read", "read");
+            btnRead.innerText = this.read ? "Not Read" : "Read";
+            this.toggleRead();
+        });
+        const tdBtnRead = document.createElement("td");
+        tdBtnRead.appendChild(btnRead);
+        tr.appendChild(tdBtnRead);
+
+        //create button delete
+        const btnDelete = document.createElement("button");
+        btnDelete.innerText = "X";
+        btnDelete.setAttribute("class", "cancelBtn");
+        btnDelete.setAttribute('data-index-number', this.id);
+        btnDelete.addEventListener('click', (e) => {
+            this.deleteBook(parseInt(btnDelete.dataset.indexNumber));
+        });
+
+        tr.appendChild(btnDelete);
+
+        tr.setAttribute('id', "book" + this.id);
+
+        tableBody.appendChild(tr);
+    }
+
+    deleteBook(index) {
+        let bookRow = document.getElementById('book' + index);
+        bookRow.parentNode.removeChild(bookRow);
+        let indexBook = myLibrary.findIndex(book => book.id === this.id);
+        myLibrary.splice(indexBook, 1);
+    }
 }
 
 const book1 = new Book("title1", "author1", "1", true);
@@ -24,9 +73,9 @@ const book3 = new Book("title3", "author3", "3", false);
 
 
 //Sample data
-addBookToLibrary(book1);
-addBookToLibrary(book2);
-addBookToLibrary(book3);
+book1.addBookToLibrary();
+book2.addBookToLibrary();
+book3.addBookToLibrary();
 
 //manage modal for new book
 const newBookBtn = document.getElementById("newBookBtn");
@@ -43,56 +92,7 @@ newBookBtn.addEventListener('click', () => {
         e.preventDefault();
         e.stopImmediatePropagation();
         const newBook = new Book(newBookTitle.value, newBookAuthor.value, newBookPages.value, newBookRead.checked);
-        addBookToLibrary(newBook);
+        newBook.addBookToLibrary();
         newBookDialog.close();
     });
 });
-
-function addBookToTable(book) {
-    const bookInfo = ["title", "author", "pages", "read"];
-    const tableBody = document.getElementById("bookTableBody");
-    const tr = document.createElement("tr");
-
-    for (let i = 0; i < bookInfo.length - 1; i++) {
-        const td = document.createElement("td");
-        td.innerText = book[bookInfo[i]];
-        tr.appendChild(td);
-    }
-
-    //create button read
-    const btnRead = document.createElement('button');
-    btnRead.innerText = book.read ? "Read" : "Not Read";
-    btnRead.setAttribute("class", book.read ? "read" : "not-read");
-    btnRead.setAttribute('data-index-number', book.id);
-    btnRead.addEventListener('click', (e) => {
-        book.read ? btnRead.classList.replace("read", "not-read") : btnRead.classList.replace("not-read", "read");
-        btnRead.innerText = book.read ? "Not Read" : "Read";
-        book.toggleRead();
-    });
-    const tdBtnRead = document.createElement("td");
-    tdBtnRead.appendChild(btnRead);
-    tr.appendChild(tdBtnRead);
-
-
-    //create button delete
-    const btnDelete = document.createElement("button");
-    btnDelete.innerText = "X";
-    btnDelete.setAttribute("class", "cancelBtn");
-    btnDelete.setAttribute('data-index-number', book.id);
-    btnDelete.addEventListener('click', (e) => {
-        deleteBook(parseInt(btnDelete.dataset.indexNumber));
-    });
-
-    tr.appendChild(btnDelete);
-
-    tr.setAttribute('id', "book" + book.id);
-
-    tableBody.appendChild(tr);
-}
-
-function deleteBook(bookId) {
-    bookRow = document.getElementById('book' + bookId);
-    bookRow.parentNode.removeChild(bookRow);
-    let indexBook = myLibrary.findIndex(book => book.id === bookId);
-    myLibrary.splice(indexBook, 1);
-}
